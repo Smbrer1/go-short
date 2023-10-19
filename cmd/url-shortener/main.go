c235d3c44b34d551b2866de9dd0da95aad8bcc5a
Artem Sokolov <avsokolov@mtt.ru>
2023-10-19 14:33:52 +0300

    feat: added config and storage declaration

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+
+	"golang.org/x/exp/slog"
+
+	"github.com/Smbrer1/go-short/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages enabled")
+	// TODO: init storage
+
+	// TODO: init router
+
+	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}

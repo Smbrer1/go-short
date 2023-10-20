1feeeb1d648553fd473abc9fb1aec6e3ee112b11
Artem Sokolov <smbrer1@gmail.com>
2023-10-20 11:05:21 +0300

    feat: finished storage initiation

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -12,14 +12,25 @@ type Storage struct {
 func New(storagePath string) (*Storage, error) {
 	const op = "storage.sqlite.New"
 
-	db, err := sql.Open("sqlite3", "./url-shortener.db")
+	db, err := sql.Open("sqlite3", storagePath)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	stmt, err := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS url(
-		
-		)
+			id INTEGER PRIVATE KEY,
+			alias TEXT NON NULL UNIQUE,
+			url TEXT NON NULL);
+		CREATE INDEX IF NOT EXISTS idx_alias on url(alias)
 	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
 	return &Storage{}, nil
 }
